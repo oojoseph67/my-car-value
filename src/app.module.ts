c39baaf163fef901a295a9b8a59879b96a91a211
Joseph <oojoseph67@gmail.com>
2024-08-14 12:14:31 +0100

    db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,16 @@ import { ReportsController } from './reports/reports.controller';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
+import { ReportEntity } from './entity/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
-      synchronize: true,
+      entities: [UserEntity, ReportEntity],
+      synchronize: true, // run this in development mode only
     }),
     ReportsModule,
     UsersModule,
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { ReportsController } from './reports.controller';
+import { ReportEntity } from '../entity/report.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReportEntity])],
+  controllers: [ReportsController],
   providers: [ReportsService],
 })
 export class ReportsModule {}
