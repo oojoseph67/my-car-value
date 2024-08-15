7df7ae6fa2e960bdd625f4a17b736bda2f6f5400
Joseph <oojoseph67@gmail.com>
2024-08-15 02:18:13 +0100

    controller and service updated

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Patch,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -13,6 +24,49 @@ export class UsersController {
       password: body.password,
     });
 
+    return response;
+  }
+
+  @Get('findOne/:id')
+  findOneUser(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    const response = this.userService.findOne({ id });
+    return response;
+  }
+
+  @Get()
+  findUser(@Query('email') email: string) {
+    const response = this.userService.find({ email });
+    return response;
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+    @Body() body: Partial<CreateUserDTO>,
+  ) {
+    const response = this.userService.update({ id, userObject: body });
+    return response;
+  }
+
+  @Delete(':id')
+  removeUser(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    const response = this.userService.remove({ id });
     return response;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entity/user.entity';
@@ -13,5 +13,42 @@ export class UsersService {
     const user = this.repo.create({ email, password }); // create instance of UserEntity
 
     return this.repo.save(user); // actually save instance of UserEntity
+  }
+
+  async findOne({ id }: { id: number }) {
+    const user = await this.repo.findOneBy({ id });
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+
+  find({ email }: { email: string }) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update({
+    id,
+    userObject,
+  }: {
+    id: number;
+    userObject: Partial<UserEntity>;
+  }) {
+    const user = await this.repo.findOneBy({ id });
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    Object.assign(user, userObject);
+    return this.repo.save(user);
+  }
+
+  async remove({ id }: { id: number }) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return this.repo.remove(user);
   }
 }
